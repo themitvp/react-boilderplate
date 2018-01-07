92d380c005efa7c1247bec69d22f7ee74dc6e69a
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2018-01-07 17:48:20 +0000

    show current env

--- src/App.js
+++ src/App.js
@@ -17,6 +17,10 @@ class App extends Component<Props> {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. Yeah
         </p>
+        <small>
+          You are running this application in <b>{process.env.NODE_ENV}</b>{" "}
+          mode.
+        </small>
       </div>
     );
   }
